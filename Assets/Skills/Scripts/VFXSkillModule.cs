80dc2e1cd9eda890f1a5f29b35d823fcce323454
Urdreaminc <antochaleo@gmail.com>
2021-11-09 20:39:28 +0300

    VFX

--- Assets/Skills/Scripts/VFXSkillModule.cs
+++ Assets/Skills/Scripts/VFXSkillModule.cs
@@ -8,7 +8,9 @@ public class VFXSkillModule : MonoBehaviour
     [SerializeField]
     private Transform _owner;
     [SerializeField]
-    private float _delay = 0.5f;
+    private float _delay = 0.6f;
+    [SerializeField]
+    private Vector3 _relativePosition = new Vector3(0f,1f,0f);
 
     public void Run()
     {
@@ -18,6 +20,6 @@ public class VFXSkillModule : MonoBehaviour
     IEnumerator DelayInstantiation()
     {
         yield return new WaitForSeconds(_delay);
-        Instantiate(_effect, _owner.position, Quaternion.identity);
+        Instantiate(_effect, _owner.position + _relativePosition,_owner.transform.rotation);
     }
 }
