33c7698cbf99785714013f7edb62a1866f3ac82b
Urdreaminc <antochaleo@gmail.com>
2021-11-20 22:49:16 +0300

    Global Update

---
+++ Assets/UI/Scripts/StatusBar/StatusPanelUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusPanelUI : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _repository = new List<GameObject>();
+    [SerializeField]
+    private float width = 120f;
+
+    public void Create(GameObject statusObject)
+    {
+        Add(statusObject);
+        RefreshPosition();
+    }
+
+    public void Add(GameObject statusObject)
+    {
+        _repository.Add(statusObject);
+    }
+
+    public void RefreshPosition()
+    {
+        for (int i = 0; i < _repository.Count; i++)
+            _repository[i].GetComponent<StatusUI>().SetPosition(i * width, 0f);
+    }
+
+    public void Remove(GameObject statusObject)
+    {
+        _repository.Remove(statusObject);
+    }
+}
---
+++ Assets/UI/Scripts/StatusBar/StatusUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusUI : MonoBehaviour
+{
+    public float duration;
+    public Image image;
+
+    private RectTransform _transform;
+
+    private void Awake()
+    {
+        _transform = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
+
+    public void SetPosition(float x, float y)
+    {
+        _transform.localPosition = new Vector3(x, y, 0f);
+    }
+
+}
